a55608383e82be7d67da162e89ef7509d2ad64ec
jcfun <t_jchf@yx.com>
2022-11-24 18:11:59 +0800

    日常更新

---
+++ Internship/Java基础/Code/Internship/_socket/src/com/jcfun/java/TCPTest1.java
@@ -0,0 +1,26 @@
+package com.jcfun.java;
+
+import org.testng.annotations.Test;
+
+/**
+ * @ClassName: TCPTest1
+ * @Package: com.jcfun.java
+ * @Author: jcfun
+ * @Date: 2022/11/24 下午4:02
+ * @Version: 1.0.0
+ * @Description: TODO
+ */
+public class TCPTest1 {
+
+    @Test
+    public void test() {
+
+
+
+
+    }
+
+
+
+
+}
